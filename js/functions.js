055a48ba5341a4f1cecfc5c6a6f5a1bda767ca3d
Arina Savko <arina_savko@mail.ru>
2023-02-25 22:55:13 +0300

    пишет функции

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,27 @@
-const checkStringLength = function (checkString, i) {
-  if (checkString.length <= i) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const checkStringLength = (checkString, maxLength) => {
+  return checkString.length <= maxLength;
+}
 checkStringLength('проверяемая строка', 20);
 checkStringLength('проверяемая строка', 18);
 checkStringLength('проверяемая строка', 10);
 
-function checkWord(word) {
-  const splitString = word.split('');
-  const reverseArray = splitString.reverse();
-  const joinArray = reverseArray.join('');
-  if (word.toLowerCase === joinArray.toLowerCase) {
-    return true;
-  } else {
-    return false;
-  }
+function isPalindrom(word) {
+  const reversedString = reverseArray.split('').reverse().join('');
+  return word.toLowerCase === reversedString.toLowerCase;
 }
-checkWord('топот');
-checkWord('ДовОд');
-checkWord('Кекс');
+isPalindrom('топот');
+isPalindrom('ДовОд');
+isPalindrom('Кекс');
 
 const extractNumber = (string) => {
   let result = '';
+
   for (let i = 0; i < string.length; i++) {
     if (!Number.isNaN(parseInt(string.at(i), 10))) {
       result += string.at(i);
     }
   }
+
   return parseInt(result, 10);
 };
 extractNumber('2023 год');

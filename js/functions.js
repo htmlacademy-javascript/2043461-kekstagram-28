703cf0407f4b8a085b26590739ac5b0b5c7980b7
Arina Savko <arina_savko@mail.ru>
2023-02-25 10:31:06 +0300

    пишет функции

--- js/functions.js
+++ js/functions.js
@@ -4,19 +4,24 @@ const checkStringLength = function (checkString, i) {
   } else {
     return false;
   }
-};
+}
+console.log(checkStringLength('проверяемая строка', 20));
+console.log(checkStringLength('проверяемая строка', 18));
+console.log(checkStringLength('проверяемая строка', 10));
 
-
-const checkWord = function (word) {
-  let splitString = word.split("");
-  let reverseArray = splitString.reverse();
-  let joinArray = reverseArray.join("");
-  if (word.toLowerCase == joinArray.toLowerCase) {
+function checkWord(word) {
+  const splitString = word.split('');
+  const reverseArray = splitString.reverse();
+  const joinArray = reverseArray.join('');
+  if (word.toLowerCase === joinArray.toLowerCase) {
     return true;
   } else {
     return false;
   }
 }
+console.log(checkWord('топот'));
+console.log(checkWord('ДовОд'));
+console.log(checkWord('Кекс'));
 
 const extractNumber = (string) => {
   let result = '';
@@ -26,7 +31,12 @@ const extractNumber = (string) => {
     }
   }
   return parseInt(result, 10);
-}
+};
+console.log(extractNumber('2023 год'));
+console.log(extractNumber('ECMAScript 2022'));
+console.log(extractNumber('1 кефир, 0.5 батона'));
+console.log(extractNumber('агент 007'));
+console.log(extractNumber('а я томат'));
 
 const padStart = (string, minLength, pad) => {
   const actualPad = minLength - string.length;
@@ -34,4 +44,9 @@ const padStart = (string, minLength, pad) => {
     return string;
   }
   return pad.slice(0,actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
-}
+};
+console.log(padStart('1', 2, '0'));
+console.log(padStart('1', 4, '0'));
+console.log(padStart('q', 4, 'werty'));
+console.log(padStart('q', 4, 'we'));
+console.log(padStart('qwerty', 4, '0'));
